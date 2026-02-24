b98036339f51b8a269e15887cc64ea43c7cb3022
Linnie Naryshkin <linnie_naryshkin@epam.com>
2026-02-24 13:35:06 +0200

    makeGood

---
+++ problems/makeGood.cpp
@@ -0,0 +1,47 @@
+#include <cassert>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+clang++ -std=c++17 problems/makeGood.cpp && ./a.out
+*/
+
+/**
+ * @problem Make Good
+ * @link
+ * https://leetcode.com/problems/make-the-string-great
+ * Time: O(n)
+ * Space: O(1)
+ */
+class Solution {
+public:
+  string makeGood(string s) {
+    string result;
+
+    for (char c : s) {
+      if (!result.empty() && abs(result.back() - c) == 32) {
+        result.pop_back();
+      } else {
+        result.push_back(c);
+      }
+    }
+
+    return result;
+  }
+};
+
+int main() {
+  Solution sol;
+
+  string s = "abBAD";
+  string result = sol.makeGood(s);
+  assert(result == "D");
+
+  s = "kaAbBcCE";
+  result = sol.makeGood(s);
+  assert(result == "kE");
+
+  cout << "All tests passed!" << endl;
+  return 0;
+}
